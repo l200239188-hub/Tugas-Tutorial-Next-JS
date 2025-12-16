ad71eac5de49c696d08b586d0fe40507a450a3a7
l200239188-hub <l200239188@student.ums.ac.id>
2025-12-16 17:19:57 +0700

    chapter 9

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,13 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-
-type CardWrapperProps = {
-  numberOfCustomers: number;
-  numberOfInvoices: number;
-  totalPaidInvoices: string;
-  totalPendingInvoices: string;
-};
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -20,12 +14,14 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default function CardWrapper({
-  numberOfCustomers,
-  numberOfInvoices,
-  totalPaidInvoices,
-  totalPendingInvoices,
-}: CardWrapperProps) {
+export default async function CardWrapper() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
@@ -36,7 +32,7 @@ export default function CardWrapper({
   );
 }
 
-export function Card({
+function Card({
   title,
   value,
   type,
@@ -50,12 +46,12 @@ export function Card({
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+        {Icon && <Icon className="h-5 w-5 text-gray-700" />}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
+
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,18 +2,22 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
 import { LatestInvoice } from '@/app/lib/definitions';
 
-export default function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+export default async function LatestInvoices() {
+  const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
+
+  if (!latestInvoices || latestInvoices.length === 0) {
+    return <p className="mt-4 text-gray-400">No invoices available.</p>;
+  }
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
+
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => (
@@ -26,7 +30,7 @@ export default function LatestInvoices({
             >
               <div className="flex items-center">
                 <Image
-                  src={invoice.image_url ?? '/placeholder.png'} // default image
+                  src={invoice.image_url ?? '/placeholder.png'}
                   alt={`${invoice.name}'s profile picture`}
                   className="mr-4 rounded-full"
                   width={32}
@@ -41,6 +45,7 @@ export default function LatestInvoices({
                   </p>
                 </div>
               </div>
+
               <p
                 className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
               >
@@ -49,6 +54,7 @@ export default function LatestInvoices({
             </div>
           ))}
         </div>
+
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
