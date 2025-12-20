b7b099e04092d4c58d9b240fed8159907d89f923
l200239188-hub <l200239188@student.ums.ac.id>
2025-12-20 16:32:24 +0700

    feat: Mengimplementasikan autentikasi pengguna dengan NextAuth, termasuk form login, Credentials Provider, proteksi route, dan fungsi sign-out

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -93,3 +93,28 @@ export async function deleteInvoice(id: string) {
     throw new Error('Failed to delete invoice');
   }
 }
+
+// ===========================
+// Authentication
+// ===========================
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
