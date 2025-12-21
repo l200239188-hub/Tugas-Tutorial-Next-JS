28bcf9a69c2e5945057c2f00aaea87cdd12052f8
l200239188-hub <l200239188@student.ums.ac.id>
2025-12-21 22:26:39 +0700

    chore: update test user credentials

--- auth.ts
+++ auth.ts
@@ -16,9 +16,9 @@ const testUsers = [
     },
     {
         id: '550e8400-e29b-41d4-a716-446655440000',
-        name: 'Ihsan',
-        email: 'ihsanbe05@gmail.com',
-        password: 'Ponorog0?',
+        name: 'Baihaqi',
+        email: 'L200239188@student.ums.ac.id',
+        password: 'baihaqi2125',
     },
 ];
 
